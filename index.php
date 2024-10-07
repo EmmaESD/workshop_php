2eb693c1f541117ddbed51c44d9204d34ab42646
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-07 14:45:41 +0200

    add code to show errors

--- index.php
+++ index.php
@@ -1,60 +1,62 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class VendorMachine {
     private $isOn;
     private $snacksQty;
     private $money;
 
-    public funtion __construct(){
+    public function __construct(){
         $this->isOn = false;
         $this->snacksQty = 50;
         $this->money = 0;
-    };
+    }
 
-    public function buySnacks(){
+    public function buySnacks(): void{
         $this->isOn = true;
-        if ($this->snacksQty < 1){
-            throw new Error('machine vide !')
-        };
+        if ($this->snacksQty < 1) {
+            throw new Error(message: 'machine vide !');
+        }
         $this->money = +2;
-    };
+    }
 
-    public function reset() {
+    public function resetVendor(): void {
         $this->isOn = false;
         $this->snacksQty = $this->calculateLeftSnacksQty();
         $this->money = 0;
         $this->isOn = true;
-    };
+    }
 
-    private function calculateLeftSnackQty() {
-        echo $this->snacksQty + (50 - $this->snacksQty);
-    };
+    private function calculateLeftSnacksQty(): int {
+        return $this->snacksQty + (50 - $this->snacksQty);
+    }
 
-    public function shootWithFoot() {
+    public function shootWithFoot(): void {
         $this->isOn = false;
-        $this->dropMoney;
-    };
+        $this->dropMoney();
+    }
 
-    private function dropMoney() {
+    private function dropMoney(): void {
         $moneyToDrop = 20;
 
         if($this->money < 20) {
-            $moneyToDrop = $this->money
-        };
+            $moneyToDrop = $this->money;
+        }
         $this->money = $this->money - $moneyToDrop;
-    };
+    }
 
-    private dropSnacks() {
+    private function dropSnacks(): void {
         $snacksToDrop = 5;
 
         if ($this->snacksQty < 5) {
-            $snacksToDrop = $this->snacksQty
-        };
-    };
+            $this->snacksQty = $this->snacksQty - $snacksToDrop;
+        }
+    }
 
 };
 
 $vendorMachine = new VendorMachine();
 
-vendorMachine.reset();
-vendorMachine.shootWithFoot();
-vendorMachine.buySnacks();
+echo $vendorMachine.snacksQty();
