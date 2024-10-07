e79a4b2f1f49731db03fd3ff52059b28b1ded4a9
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-07 16:41:44 +0200

    add correction

--- Order.php
+++ Order.php
@@ -9,6 +9,10 @@
 - Remplir l'adresse de livraison
 - Payer
 - Annuler
+
+Regles métier (exceptions, règles faites par les chefs de projet):
+- max 5 produits par commande,
+- Bannissement de "jean dupont"
 */
 
 ini_set('display_errors', 1);
@@ -26,20 +30,39 @@ class Order {
     private string $status;
 
     public function __construct(string $customerName, array $products) {
+        if($customerName === 'jean dupont'){
+            throw new Exception(message: 'Achat impossible, vous êtes blacklisté');
+        }
+        if(count($products) > 5) {
+            throw new Exception(message: 'Panier complet');
+        }
+        
         $this->status = 'CART';
         $this->createdAt = new DateTime();
         $this->id = rand();
 
         $this->products = $products;
-        $this->customerName = $customerName;
 
+        $this->customerName = $customerName;
+        
+        
         $this->totalPrice = count($products) * 5;
 
-        echo "Commande {$this->id} créée d'un montant de {$this->totalPrice} €";
+        
 
+        echo "Commande {$this->id} créée d'un montant de {$this->totalPrice} €";
     }
 }
 
-$order = new Order(customerName:'John Doe', products:['casque', 'téléphone']);
+
+//Methode du try catch qui récupère le message pour l'afficher plutot que de mettre une erreur
+//permet de ne pas faire crasher la page avec les exception.
+try {
+    $order = new Order(customerName:'jean dupont', products:['casque', 'téléphone','casque', 'téléphone','casque', 'téléphone']);
+} catch(Exception $error) {
+    echo $error->getMessage();
+}
+
+
 
 
